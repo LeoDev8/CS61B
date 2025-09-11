4bd17351b91b5a8bb39c5a836a38d354ad8ca209
Leo <zhihao_2023@613.com>
2025-09-11 17:55:40 +0800

    test_module2

---
+++ test_module/src/Dog.java
@@ -0,0 +1,14 @@
+public class Dog {
+    String name;
+    Integer weight;
+
+    public void sayHello(String args) {
+        System.out.println(args);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello From Dog Class.\n");
+        Dog dog1 = new Dog();
+        dog1.sayHello("Wooo Wang, dog1");
+    }
+}
