5e1fd6cf751298e5dacc9532bbbfc030cf7e9573
Leo <zhihao_2023@613.com>
2025-09-11 17:57:51 +0800

    Add dog2

--- test_module/src/Dog.java
+++ test_module/src/Dog.java
@@ -10,5 +10,8 @@ public class Dog {
         System.out.println("Hello From Dog Class.\n");
         Dog dog1 = new Dog();
         dog1.sayHello("Wooo Wang, dog1");
+
+        Dog dog2 = new Dog();
+        dog2.sayHello("Hi, I'm dog2");
     }
 }
